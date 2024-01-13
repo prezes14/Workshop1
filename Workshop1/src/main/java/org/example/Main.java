750aede220393f38b03f57b77ea2ba8cb99f6b73
prezes14 <playdamiano@gmail.com>
2024-01-13 17:05:39 +0100

    1

--- Workshop1/src/main/java/org/example/Main.java
+++ Workshop1/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        public static void printOptions(String[] tab) {
+//
+//            System.out.println(ConsoleColors.BLUE);
+//
+//            System.out.println("Please select an option: " + ConsoleColors.RESET);
+//
+//            for (String option : tab) {
+//
+//                System.out.println(option);
+//
+//            }
+//
+//        }
+
+        System.out.println(ConsoleColors.BLUE+"Please select an option:");
+        System.out.println(ConsoleColors.RESET+"add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+
     }
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    static String[][] tasks;
+
 }
