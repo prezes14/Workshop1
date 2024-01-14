92dd9bd00479f0ef2d8abc96c7a8f59792f1a329
prezes14 <playdamiano@gmail.com>
2024-01-14 15:35:19 +0100

    1

--- Workshop1/src/main/java/org/example/Main.java
+++ Workshop1/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,12 +8,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static final String FILE_NAME = "tasks.csv";
     static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    public static void Menu (String[] MENU2) {
+        for (int i = 0; i < OPTIONS.length; i++) {
+            System.out.println(ConsoleColors.RESET + OPTIONS[i]);
+        }
+    }
+
     static String[][] tasks;
 
     public static void main(String[] args) {
@@ -31,10 +40,7 @@ public class Main {
 //
 //        }
         System.out.println(ConsoleColors.BLUE + "Please select an option:");
-        System.out.println(ConsoleColors.RESET + "add");
-        System.out.println("remove");
-        System.out.println("list");
-        System.out.println("exit");
+        Menu((OPTIONS));
         tasks = loadData(FILE_NAME);
         tasks = Dane(FILE_NAME);
     }
@@ -59,38 +65,71 @@ public class Main {
         } catch (IOException ex) {
             ex.printStackTrace();
             System.out.println("Program error");
-        }        return tab;
+        }
+        return tab;
     }
 
     public static String[][] Dane(String File) {
 
         Path path1 = Paths.get("/home/prezes14/Documents/Workshop1/Workshop1/tasks.csv");
-        System.out.println(Files.exists(path1));
+
+
         try (Scanner scanner = new Scanner(System.in)) {
-            String task_name = scanner.nextLine();
-            if (task_name.equals("add"))
-                System.out.println("Please add task description");
+//            while (scanner.hasNextLine()) {
+//                String input = scanner.nextLine();
+//                switch (input) {
+//                    case "exit":
+//                        break;
+//                    case "add":
+//                        break;
+//                    case "remove":
+//                        break;
+//                    case "list":
+//                        break;
+//                    default:
+//                        System.out.println("Insert correct task");
+//                }
+//           }
+            String input_name = scanner.nextLine();
 
             String tab[][] = tasks;
-
-            for (int i = 0; i < tab.length; i++) {
-                String task_name2 = scanner.nextLine();
-                Files.writeString(path1, task_name2, StandardOpenOption.APPEND);
-                for (int j = 0; j < tab[i].length; j++) {
-                    System.out.println("Insert date:");
-                    String date_name = scanner.nextLine();
-                    Files.writeString(path1, date_name, StandardOpenOption.APPEND);
-                    System.out.println("Insert importance: (true/false)");
-                    String Importance = scanner.nextLine();
-                    Files.writeString(path1, Importance, StandardOpenOption.APPEND);
+            if (input_name.equals("add")) {
+                for (int i = 0; i < tab.length; i++) {
+                    System.out.println("Please add task description");
+                    String task_name = "\n" + scanner.nextLine() + ", ";
+                    Files.writeString(path1, task_name, StandardOpenOption.APPEND);
+                    for (int j = 0; j < tab[i].length; j++) {
+                        System.out.println("Insert date:");
+                        String date_name = scanner.nextLine() + ", ";
+                        Files.writeString(path1, date_name, StandardOpenOption.APPEND);
+                        System.out.println("Insert importance: (true/false)");
+                        String Importance = scanner.nextLine();
+                        Files.writeString(path1, Importance, StandardOpenOption.APPEND);
+                        Menu((OPTIONS));
+                    }
                 }
-            }
+            } else if (input_name.equals("list")) {
+
+                for (int i =0; i < tasks.length; i++) {
+                    System.out.println();
+                    for (int j = 0; j < tasks[i].length; j++) {
+                        System.out.print(Arrays.asList(tasks[i][j]));
+
+                    }
+                }
+
+            } else if (input_name.equals("remove")) {
+
+            } else if (input_name.equals("exit")) {
+                System.out.println("Bye, bye babe");
+        } else System.out.println("Insert correct task");
             String task2 = scanner.nextLine();
             Files.writeString(path1, task2, StandardOpenOption.APPEND);
+            Menu((OPTIONS));
         } catch (IOException ex) {
-            System.out.println("Brak mozliwosci zapisu do pliku");
+            System.out.println("Cannot save the file");
         }
         return tasks;
-//        if
-//        System.out.println("Add task due date");
-    }}
+
+    }
+}
